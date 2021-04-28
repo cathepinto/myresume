d5d8773f66c913bb7058cb582558e78e5ebc4fd1
Catarina Amorim Pinto <79118496+cathepinto@users.noreply.github.com>
2021-04-28 13:42:21 +0100

    Add files via upload

---
+++ contact_form_handler.php
@@ -0,0 +1,24 @@
+<?php
+    $name = $_Post['name'];
+    $visitor_email = $Post['email'];
+    $message = $Post['message'];
+
+    $email_from = 'catarinapinto1991@gmail.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                    "User Message": "$message.\n";
+
+    $to = 'catarinapinto1991@gmail.com';
+
+    $headers "From: $email_from \r\n";
+
+    $headers .= "Reply-to $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+?>
